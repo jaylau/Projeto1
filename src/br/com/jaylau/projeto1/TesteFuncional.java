0a0d61c369200a2c9f2fe2d88359dd7605761d80
JHLau <jeffhyl@hotmail.com>
2012-10-01 17:17:08 -0300

    Criado mais uma classe

---
+++ src/br/com/jaylau/projeto1/TesteFuncional.java
@@ -0,0 +1,13 @@
+package br.com.jaylau.projeto1;
+
+public class TesteFuncional {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Erro!");
+	}
+
+}
